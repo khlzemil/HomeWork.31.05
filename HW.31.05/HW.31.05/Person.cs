27aa1c25091e212af4ee89a139b3f0ed96b83b55
khlzemil <emilmg@code.edu.az>
2022-05-31 08:41:09 +0400

    Added HomeTask

---
+++ HW.31.05/HW.31.05/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._31._05
+{
+    internal class Person
+    {
+
+        public static int stAge;
+
+        public int age;
+        public Person()
+        {
+            stAge++;
+            age = stAge;
+        }
+        
+    }
+}
---
+++ HW.31.05/HW.31.05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW._31._05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Person tamerlan = new Person();
+            Console.WriteLine(tamerlan.age); 
+            Person faiq = new Person();
+            Console.WriteLine(faiq.age);
+
+            Person tural = new Person();
+            Console.WriteLine(tural.age); 
+
+            Person emil = new Person();
+            Console.WriteLine(emil.age);
+        }
+    }
+}
